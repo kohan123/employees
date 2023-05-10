e893bffe54a15b402f33fa8684cfa149bc4268eb
kohan123 <sportodavka@gmail.com>
2023-05-10 18:28:24 +0200

    feat: create new blocks

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,10 @@
 import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployeesAddForm from '../employers-add-form/emloyers-add-form';
+
 
 function App () {
     return (
@@ -9,7 +13,10 @@ function App () {
 
             <div className='search-panel'>
                 <SearchPanel />
+                <AppFilter/>
             </div>
+            <EmployersList/>
+            <EmployeesAddForm/>
         </div>
     );
 };
