5e09ee092b5f039676a7bfb4b0df08a63b9e48ac
kohan123 <sportodavka@gmail.com>
2023-05-11 20:13:22 +0200

    feat: create dynamic list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employers-add-form/emloyers-add-form';
 
 
 function App () {
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: false},
+        {name: 'Alex M.', salary: 3000, increase: true},
+        {name: 'Carl D.', salary: 5000, increase: false},
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -15,7 +22,7 @@ function App () {
                 <SearchPanel />
                 <AppFilter/>
             </div>
-            <EmployersList/>
+            <EmployersList data={data}/>
             <EmployeesAddForm/>
         </div>
     );
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,12 +1,17 @@
 import EmployeesListItem from "../employers-list-item/employers-list-item";
 import './employers-list.css';
 
-const EmployersList = () => {
+const EmployersList = ({data}) => {
+
+const element = data.map((item) => { 
+    return (
+     <EmployeesListItem {...item}/>  //name = {item.name} salary= {item.salary}
+    );
+})
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {element}
         </ul>
     );
 }
