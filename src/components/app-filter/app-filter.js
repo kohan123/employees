d9d59f998a65c7b1d90a66bd6509fe4199aeaef1
kohan123 <sportodavka@gmail.com>
2023-05-22 19:58:49 +0200

    feat: add search of eployees

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,22 +1,29 @@
 import './app-filter.css';
+import { Component } from 'react';
 
-const AppFilter = () => {
-    return (
-        <div className="btn-group">
-            <button 
-            className="btn btn-light"
-            type="button"
-            >Все сотрудники</button>
-            <button 
-            className="btn btn-outline-light"
-            type="button"
-            >На повышение</button>
-            <button 
-            className="btn btn-outline-light"
-            type="button"
-            >З/П больше 1000$</button>
-        </div>
-    );
+class AppFilter extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="btn-group">
+                <button 
+                className="btn btn-light"
+                type="button"
+                >Все сотрудники</button>
+                <button 
+                className="btn btn-outline-light"
+                type="button"
+                >На повышение</button>
+                <button 
+                className="btn btn-outline-light"
+                type="button"
+                >З/П больше 1000$</button>
+            </div>
+        );
+    }
+   
 }
 
 export default AppFilter;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
             {name: 'John C.', salary: 800, increase: false, like: true, id: 1},
             {name: 'Alex M.', salary: 3000, increase: true, like: false, id: 2},
             {name: 'Carl D.', salary: 5000, increase: false, like: false, id: 3}
-        ]
+        ],
+        term: ''
     
     }
     this.maxId = 4;
@@ -57,11 +58,30 @@ onToggleProp = (id, prop) => {
     }))
 }
 
+searchEnp = (items, term) => {
+    if(items.length === 0) {
+        return items;
+    }
+
+    return items.filter(item => {
+        return item.name.indexOf(term) > -1
+    })
+}
+
+onUpdateSearch = (term) => {
+    this.setState({term});
+}
+
+
+
 
 
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length
+        const visibleData = this.searchEnp(data, term);
+
         return (
             <div className="app">
                 <AppInfo
@@ -69,11 +89,11 @@ onToggleProp = (id, prop) => {
                 increased={increased}/>
     
                 <div className='search-panel'>
-                    <SearchPanel />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                     <AppFilter/>
                 </div>
                 <EmployersList 
-                data={this.state.data}
+                data={visibleData}
                 onDelete={this.deletItem}
                 onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAddNewItem={this.addItem}/>
